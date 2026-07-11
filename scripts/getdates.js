98ca8e053624a00749dc32aa6605637644a1a8d0
echevarria777arroyo-sys <echevarria777arroyo@gmail.com>
2026-07-11 16:23:38 -0500

    Add files via upload

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Mostrar el año actual en el footer
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Mostrar la fecha de última modificación del documento
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
