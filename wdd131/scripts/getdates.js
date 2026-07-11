a612b71c59e0e29aa224c0dd3abf70d196a9a3ed
echevarria777arroyo-sys <echevarria777arroyo@gmail.com>
2026-07-11 16:27:12 -0500

    Delete wdd131 directory

--- wdd131/scripts/getdates.js
+++
@@ -1,5 +0,0 @@
-// Mostrar el año actual en el footer
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-// Mostrar la fecha de última modificación del documento
-document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
